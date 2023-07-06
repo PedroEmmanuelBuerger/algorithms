e11a00cb202e71741f12755dd52b95694ff6f41d
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-06 18:23:08 -0300

    wip 6

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -3,8 +3,6 @@ def find_duplicate(nums):
         return False
     duplicate_count = []
     for num in nums:
-        if not isinstance(num, int) or num < 0:
-            return False
         if num in duplicate_count:
             return num
         else:
