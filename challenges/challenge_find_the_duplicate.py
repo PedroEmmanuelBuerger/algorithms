69ae6a48b4055f48d14fca836a8047090856cf34
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-06 17:59:11 -0300

    requesito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) == 0:
+        return False
+    duplicate_count = []
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in duplicate_count:
+            return num
+        else:
+            duplicate_count.append(num)
+    return False
