70909c911b58bb304651c11193f40a2dfbfb368b
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-06 18:47:49 -0300

    requesito 5 finalizando projeto

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,10 +1,25 @@
 def find_duplicate(nums):
     if len(nums) == 0:
         return False
-    duplicate_count = []
+    if not verify_numbers(nums):
+        return False
+    duplicate_count = {}
     for num in nums:
         if num in duplicate_count:
             return num
         else:
-            duplicate_count.append(num)
+            duplicate_count[num] = 1
     return False
+
+
+def verify_numbers(nums):
+    try:
+        for num in nums:
+            if num < 0 or type(num) != int:
+                return False
+        return True
+    except TypeError:
+        return False
+
+
+print(find_duplicate(["a", "b"]))
