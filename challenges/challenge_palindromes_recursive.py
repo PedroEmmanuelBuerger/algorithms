74695a3fb4f081fb58948d68aa13d563504d3a9c
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-06 16:41:43 -0300

    requesito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    if low_index == high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index < high_index + 1:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
