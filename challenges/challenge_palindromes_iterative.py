c206f0845f7bc2577f8dc41da943ae305b905fbb
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-06 17:25:28 -0300

    requesito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    reverse_Word = ''
+    for i in range(len(word)):
+        reverse_Word += word[len(word)-1-i]
+    return reverse_Word == word
