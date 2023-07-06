41029d0247edd09df8a033a79fbba0fd5f3c4e92
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-07-06 17:04:35 -0300

    wip req4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,18 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    first_string = order_alphabet_string(first_string)
+    second_string = order_alphabet_string(second_string)
+    return first_string, second_string, first_string == second_string
+
+
+def order_alphabet_string(string):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    ordered_string = ""
+    for letter in alphabet:
+        if letter in string:
+            ordered_string += letter
+    return ordered_string
+
+
+print(is_anagram("pedra", "perdaaa"))
